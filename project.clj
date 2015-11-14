153e466d3b3f0bd31218f17b7493123eb2c2dda2
Rachel Fettig <rachelfettig@Korriban.local>
2015-11-14 14:03:54 -0600

    separating HTML from app-routes

--- project.clj
+++ project.clj
@@ -4,9 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler chatter.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
+
+
